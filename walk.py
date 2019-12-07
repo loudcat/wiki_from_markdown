12c04aabf223f35698c985ca877d57d90333e5cc
loudcat <jonikwm@gmail.com>
2019-12-08 01:33:52 +0500

    add comment and format walk.py

--- walk.py
+++ walk.py
@@ -1,4 +1,4 @@
-import os
+""" functions for work with directory """
 import re
 import time
 from pathlib import Path
@@ -16,13 +16,14 @@ def branch(func):
 
 
 def get_tree(_dir, level=0):
-    node = []
+    """ Return tree of directory for sidebar menu """
+    node = list()
     for item in Path(_dir).iterdir():
         if item.is_file():
             node.append({'name': item.name, 'type': 'file', 'link': '/' + item.as_posix()})
-            print(f'{"-" * level}{item.name}')
+            #print(f'{"-" * level}{item.name}')
         else:
-            print(f'{"-" * level}d\'{item.name}')
+            #print(f'{"-" * level}d\'{item.name}')
             node.append(
                 {'name': item.name, 'type': 'dir', 'parent': get_tree(item, level=level + 1)})
     return node
@@ -31,28 +32,24 @@ def get_tree(_dir, level=0):
 @branch
 def search_in_files(_dir, word):
     reg_exp = re.compile(r'({})'.format(word), flags=re.IGNORECASE)
-    res = []
-    for file in Path(_dir).rglob('*.*'):
+    response = []
+    for file in Path(_dir).rglob('*.md'):
         # if r.search(file.name):
-        #     res.append({'file': r.sub(r'<b>\1</b>',file.name), 'lines': []})
+        #     response.append({'file': r.sub(r'<b>\1</b>',file.name), 'lines': []})
 
-        if os.stat(str(file)).st_size > 0:
-            with open(str(file)) as open_file:  # , mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as s:
+        if file.stat().st_size > 0:
+            with file.open() as open_file:
                 for line in open_file.readlines():
                     if reg_exp.search(line):
                         try:
-                            fa = next(item for item in res if reg_exp.sub(r'<b>\1</b>', str(file)) == item['file'])
+                            fa = next(item for item in response if reg_exp.sub(r'<b>\1</b>', str(file)) == item['file'])
                             fa['lines'].append(reg_exp.sub(r'<b>\1</b>', line))
                         except StopIteration:
-                            res.append(
-                                {'file': reg_exp.sub(r'<b>\1</b>', str(file)),
-                                 'href': '/' + str(file),
-                                 'lines': [reg_exp.sub(r'<b>\1</b>', line)]}
+                            response.append(
+                                {
+                                    'file': reg_exp.sub(r'<b>\1</b>', str(file)),
+                                    'href': '/' + str(file),
+                                    'lines': [reg_exp.sub(r'<b>\1</b>', line)]
+                                }
                             )
-    return res
-
-# if __name__ == '__main__':
-#     # node = get_tree('example_dir')
-#     # print(node)
-#     r = search_in_files('example_dir', 'test')
-#     print(r)
+    return response
